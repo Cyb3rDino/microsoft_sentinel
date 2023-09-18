634302b3a26bbce12df993443c8dfb6f92d20d31
Torsten.Holm@softwareone.com <Torsten.Holm@softwareone.com>
2023-09-18 15:57:13 +0200

    Updatedt CEF_Traffic_sim.bash

--- tools/CEF_Logs/CEF_Traffic_sim.bash
+++ tools/CEF_Logs/CEF_Traffic_sim.bash
@@ -1,14 +1,41 @@
 #!/bin/bash
-#Welcome Message
-echo CEF Traffic Simulator ist starting.
-#Get amount of entries to send
-read -p "How many entries do you want to send?" b
-#Define Facility
-read -p "Which Facility ?" fa
-f="local$fa.warn"
-#Define var
+
+# Welcome Message
+echo "CEF Traffic Simulator is starting."
+
+# Define if CEF or Syslog
+read -p "Do you want to send CEF or Syslog? (1=CEF, 2=Syslog): " d
+
+# Get the number of entries to send
+read -p "How many entries do you want to send? " b
+
+# Define variables
 i=1
 t=$(date "+%T")
+
+
+# While Loop for sending CEF Entries
+if [ "$d" == "1" ]; then
+   # Define Facility for CEF
+   read -p "Which Facility? " fa
+   f="local$fa.warn"
+
+   while [ $i -le $b ]; do
+      echo "Message Number: $i was sent."
+      logger -p $f -P 514 -n 127.0.0.1 --rfc3164 -t CEF "0|Mock-test$fa|TRAFFIC-Simulator|common=event-format-test numb>"
+      ((i++))
+      sleep 10
+   done
+else
+   # Syslog Simulation
+   while [ $i -le $b ]; do
+      echo "Message Number: $i was sent."
+      logger -p local7.warn -P 514 -n 127.0.0.1 "helloW0rld Number$i"
+      ((i++))
+      sleep 10
+   done
+fi
+
 #while Loop for sendind CEF Entries
 while [ $i -le $b ]
 do
@@ -17,3 +44,4 @@ do
   ((i++))
   sleep 10
 done
+
